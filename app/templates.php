7021c79581696003eff957a64eaf21f13714a9e2
ankhang1704 <ankhang1704@gmail.com>
2019-05-14 16:55:18 +0700

    layout index

--- app/templates.php
+++ app/templates.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class tamplates extends Model
+class templates extends Model
 {
     protected $table = "templates";
+    public function services(){
+      return $this->hasmany('App\list_services','templates_id','id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('index',['as'=>'trang-chu','uses'=>'PageController@getIndex']);
